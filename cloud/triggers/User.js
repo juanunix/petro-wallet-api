301b351b53bc5cd8a906f7d5045361ecd3adf9dd
MutterPedro <pedro.mutter@gmail.com>
2017-10-01 10:48:42 -0300

    correçãO

--- cloud/triggers/User.js
+++ cloud/triggers/User.js
@@ -3,32 +3,33 @@ Parse.Cloud.beforeSave(Parse.User, function(request, response) {
 
 	if(user.isNew()) {
         user.set("balance", 0);
-        response.success();
-        return;
     }
 
-	if(user.dirty('balance')) {
-		let query = new Parse.Query(Parse.User);
-		query.get(user.id).then((oldUser) => {
-			if (!oldUser) {
-				response.success();
-				return;
-			}
-			const value = oldUser.get("balance") - user.get("balance");
+    response.success();
+});
 
-			let history = new Parse.Object("History");
-			history.set("user", user);
-			history.set("value", value);
+Parse.Cloud.afterSave(Parse.User, function(request) {
+    var user = request.object;
+    if(user.dirty('balance')) {
+        let query = new Parse.Query(Parse.User);
+        query.get(user.id).then((oldUser) => {
+            if (!oldUser) {
+                response.success();
+                return;
+            }
+            const value = oldUser.get("balance") - user.get("balance");
 
-			return history.save();
-		}).then(() => {
-			response.success();
-		}, (error) => {
-			response.error(error);
-		});
-	} else {
-		response.success();
-	}
+            let history = new Parse.Object("History");
+            history.set("user", user);
+            history.set("value", value);
+
+            return history.save();
+        }).then(() => {
+            console.log("FOOOOI #############");
+        }, (error) => {
+            console.error(error);
+        });
+    } 
 });
 
 Parse.Cloud.beforeSave("Session", function (request, response) {
